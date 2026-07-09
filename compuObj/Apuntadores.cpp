318a60cc887c10921652de1e383d5f15bb082dac
Fernanda Espinoza <a01742587@tec.mx>
2026-07-09 17:34:28 +0000

    Apuntadores

---
+++ compuObj/Apuntadores.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+   
+    int valor=9;
+   
+        cout<<"Valor de la variable: "<<valor<<endl;
+    cout<<"Dirección de la variable: "<<&valor;
+   
+   
+    return 0;
+}
